ffec25adff1f4a007982784107c449cf5f7e606d
James <kamaujames3705@gmail.com>
2023-11-04 00:37:15 +0300

    ✨ feat:created figurecards component file

---
+++ src/Components/FigureCards.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function FigureCards() {
+  return (
+    <div>
+            <div  className=''>
+            <div className=''>
+                <span>1.3</span>
+                <span>M</span>
+            </div>
+            <div className=''>
+                <p>
+                Approx. number of Kenyan talents reaching the age of 18 in 2022
+                </p>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default FigureCards
--- src/Components/Figures.js
+++ src/Components/Figures.js
@@ -5,8 +5,14 @@ import '../App.css'
 function Figures() {
   return (
     <div>
+            {/* Key figures container */}
+        <div className='container flex flex-col'>
+            {/* Key figures header */}
       <div className='figures-header mx-auto text-center md:mt-20 md:text-5xl'>
         <h3>Key figures</h3>
+      </div>
+        {/* Cards */}
+
       </div>
     </div>
   )
