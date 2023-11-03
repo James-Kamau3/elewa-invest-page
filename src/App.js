005cbbc9a7648aee558413f741ec9198f62c8b1a
James <kamaujames3705@gmail.com>
2023-11-03 22:21:25 +0300

    :sparkle:added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Description from './Components/Description';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div >
       <Navbar />
+      <Description />
     </div>
   );
 }
--- src/Components/Description.js
+++ src/Components/Description.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import '../App.css'
+
 
 function Description() {
   return (
     <div>
-      
+      {/* Description Container */}
+      <div className='container flex flex-col'>
+        <div className='different-text text-xs mt-24 ml-16 md:ml-16 md:mt-20'>
+            <h1>How we see it differently</h1>
+        </div>
+        <div>
+            
+        </div>
+      </div>
     </div>
   )
 }
