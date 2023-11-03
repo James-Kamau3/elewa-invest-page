252bba29fa75b83f35ffec9dcaa2b4ad9ba82c63
James <kamaujames3705@gmail.com>
2023-11-03 18:30:19 +0300

    :lipstick:updated fonts folder

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,9 +4,10 @@ import '../App.css'
 function Navbar() {
   return (
     <div className='navbar'>
+        <img className='img' src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1679982347/elewa-group-website/hero-Images/dark-to-light_xrat5g.jpg' alt='sky'/>
       <nav className='relative container mx-auto p-12 md:border-bottom-solid md:border-bottom-white'>
         {/* Logo */}
-        <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between md:ml-6'>
         <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg' alt='logo' />
         {/* Menu Items */}
         <div className='hidden text-white md:flex md:flex-row space-x-6'>
@@ -24,10 +25,6 @@ function Navbar() {
 
         </div>
       </nav>
-      {/* Quote Section */}
-      <div className=''>
-
-      </div>
     </div>
   )
 }
