eeb3a10cf2a58b61dc6996f42a54e518357e8170
James <kamaujames3705@gmail.com>
2023-11-03 23:53:52 +0300

    ✨ feat:created figures component file

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -6,12 +6,12 @@ function Description() {
   return (
     <div>
       {/* Description Container */}
-      <div className='description-container container flex flex-col ml-16 md:pl-16 md:max-w-fit'>
+      <div className='description-container container flex flex-col pl-16 md:pl-16 md:max-w-fit'>
         <div className='different-text text-xs mt-24 md:mt-20'>
             <h1>How we see it differently</h1>
         </div>
 
-        <div className='description-text md:w-8/12 md:mt-10'>
+        <div className='description-text mt-10 md:w-8/12 md:mt-10'>
             <p>
             The global South is rapidly catching up with the "developed" world. It's
             time for you to get to know the rapid development, innovation and drive
@@ -19,7 +19,7 @@ function Description() {
             </p>
         </div>
         <div>
-            <img className='view-img md:mt-16' src='https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,w_1280/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png' alt='view'/>
+            <img className='view-img mt-8 md:mt-16' src='https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,w_1280/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png' alt='view'/>
         </div>
       </div>
     </div>
---
+++ src/Components/Figures.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Figures() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Figures
