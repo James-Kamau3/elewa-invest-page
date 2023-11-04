84d864957c66fe0966e41dabec79501a9376ba5b
James <kamaujames3705@gmail.com>
2023-11-05 00:16:14 +0300

    ✨ feat:created figure cards

--- src/Components/FigureCards.js
+++ src/Components/FigureCards.js
@@ -1,20 +1,100 @@
 import React from 'react'
+import '../App.css'
+
 
 function FigureCards() {
   return (
     <div>
                   {/* FigureCards container */}
-            <div  className='container flex flex-col mx-auto'>
+            <div  className='figurecards-container grid grid-cols-2 md:grid md:grid-cols-4 md:mt-20 md:grid-flow-row'>
                   {/* Card for displaying the cards */}
-            <div className='md:w-1/4'>
-                <span className='font-bold'>1.3</span>
-                <span>M</span>
+
+                  <div className='p-6 border-y border-black md:flex-row md:border-x md:pt-32 rounded-3xl'>
+            <div className='figures font-bold' >
+                <span className='figure'>1.3</span>
+                <span className=''>M</span>
             </div>
-            <div className=''>
+            <div className='figure-text'>
                 <p>
                 Approx. number of Kenyan talents reaching the age of 18 in 2022
                 </p>
             </div>
+            </div> 
+
+            <div className='p-6 border-y border-black md:p-8 md:border-x md:pt-32 rounded-3xl'>
+            <div className='figures font-bold' >
+                <span className='figure'>5</span>
+                <span className=''>M</span>
+            </div>
+            <div className='figure-text'>
+                <p>
+                number of East-Africans(EAC) reaching the age of 18 in 2022                </p>
+            </div>
+            </div> 
+
+            <div className='p-6 border-y border-black md:p-8 md:border-x md:pt-32 rounded-3xl'>
+            <div className='figures font-bold' >
+                <span className='figure'>2022</span>
+            </div>
+            <div className='figure-text'>
+                <p>
+                Democratic Republic of Congo joins the East African Community                </p>
+            </div>
+            </div> 
+
+            <div className='p-6 border-y border-black md:p-8 md:border-x md:pt-32 rounded-3xl'>
+            <div className='figures font-bold' >
+                <span className='figure'>1.5</span>
+                <span className=''>M</span>
+            </div>
+            <div className='figure-text'>
+                <p>
+                The number of unfilled IT vacancies in the US and EU markets                </p>
+            </div>
+            </div> 
+
+            <div className='p-6 border-y border-black md:p-8 md:border-x md:pt-32 rounded-3xl'>
+            <div className='figures font-bold' >
+                <span className='figure'>10</span>
+                <span className=''>M</span>
+            </div>
+            <div className='figure-text'>
+                <p>
+                Number of technical vacancies by 2030                </p>
+            </div>
+            </div> 
+
+            <div className='p-6 border-y border-black md:p-8 md:border-x md:pt-32 rounded-3xl'>
+            <div className='figures font-bold' >
+                <span className='figure'>2050</span>
+            </div>
+            <div className='figure-text'>
+                <p>
+                Africa holds the worlds largest workforce                </p>
+            </div>
+            </div> 
+
+            <div className='p-6 border-y border-black md:p-8 md:border-x md:pt-32 rounded-3xl'>
+            <div className='figures font-bold' >
+                <span className='figure'>$4.6</span>
+                <span className=''>M</span>
+            </div>
+            <div className='figure-text'>
+                <p>
+                Startup funding raised by African startups in 2022                </p>
+            </div>
+            </div> 
+
+            <div className='p-6 border-y border-black md:p-8 md:border-x md:pt-32 rounded-3xl'>
+            <div className='figures' >
+                <span className='figure'>310</span>
+                <span className=''>%</span>
+            </div>
+            <div className='figure-text'>
+                <p>
+                Increase in startup funding for companies with HQ in Kenya                </p>
+            </div>
+            </div> 
       </div>
     </div>
   )
